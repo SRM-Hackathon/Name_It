059a5935bc4d7e6432210a937fd71da3c3f53d67
Raj Kulkarni <74099005+RajKulk16@users.noreply.github.com>
2021-03-14 11:49:29 +0530

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -3,16 +3,18 @@
 const SHA256 = require('crypto-js/sha256');
 
 class Block {
-    constructor(index, timestamp, data, previousHash = "") {
+    constructor(index, description, medical_speciality, sample_name, transcription, previousHash = "") {
         this.index = index;
-        this.timestamp = timestamp;
-        this.data = data;
+        this.description = description;
+        this.medical_speciality = medical_speciality;
+        this.sample_name = sample_name;
+        this.transcription = transcription;
         this.previousHash = previousHash;
         this.hash = this.calculateHash();
     }
 
     calculateHash() {
-        return SHA256(this.index + this.previousHash + this.timestamp + JSON.stringify(this.data)).toString();
+        return SHA256(this.index + this.previousHash + this.transcription + this.sample_name);
     }
 }
 
@@ -22,7 +24,7 @@ class Blockchain {
     }
 
     createGenesisBlock() {
-        return new Block(0, "01/01/2017", "Genesis block", "0");
+        return new Block(1, "Consult for laparoscopic gastric bypass.", "Bariatrics", "Laparoscopic Gastric Bypass Consult - 2", "He has difficulty climbing stairs, difficulty with airline seats, tying shoes, used to public seating, and lifting objects off the floor.", "0");
     }
 
     getLatestBlock() {
@@ -34,18 +36,32 @@ class Blockchain {
         newBlock.hash = newBlock.calculateHash();
         this.chain.push(newBlock);
     }
+
+    isChainValid() {
+        for (let i = 1; i < this.chain.length; i++){
+            const currentBlock = this.chain[i];
+            const previousBlock = this.chain[i-1];
+            if (currentBlock.previousHash !== previousBlock.hash) {
+                return false;
+            }
+        }
+        return true;
+    }
 }
 
-let  coin = new Blockchain();
-coin.addBlock(new Block(1, "10/07/2017", {amount: 4}));
-coin.addBlock(new Block(2, "12/07/2017", { amount: 10}));
+let  data = new Blockchain();
+data.addBlock(new Block(2, "Consult for laparoscopic gastric bypass.", "Bariatrics", "Laparoscopic Gastric Bypass Consult - 1", "He has difficulty climbing stairs, difficulty with airline seats, tying shoes, used to public seating, difficulty walking, high cholesterol, and high blood pressure.  He has asthma and difficulty walking two blocks or going eight to ten steps."));
+data.addBlock(new Block(3, "2-D M-Mode. Doppler.", "Cardiovascular / Pulmonary", "2-D Echocardiogram - 1", "Left atrial enlargement with left atrial diameter of 4.7 cm. Normal size right and left ventricle. Normal LV systolic function with left ventricular ejection fraction of 51%."));
+data.addBlock(new Block(4, "2-D Echocardiogram", "Cardiovascular / Pulmonary", "2-D Echocardiogram - 2", "The left ventricular cavity size and wall thickness appear normal. The wall motion and left ventricular systolic function appears hyperdynamic with estimated ejection fraction of 70% to 75%. There is near-cavity obliteration seen."));
+data.addBlock(new Block(5, " Morbid obesity.", "Bariatrics", "Laparoscopic Gastric Bypass", "Overweight for many years. She has tried many different diets, but is unsuccessful. Received some handouts, and signed the consent. The risks and benefits of the procedure have been explained to the patient."))
 
-console.log(JSON.stringify(coin, null, 4))
-var myJSON = JSON.stringify(coin, null, 4);
+var myJSON = JSON.stringify(data, null, 4);
+console.log(data.chain[1].hash);
+console.log(data.isChainValid());
+// var bodyParser = require('body-parser');
+// var express = require('express');
+// var app = express();
 
-var bodyParser = require('body-parser');
-var express = require('express');
-var app = express();
 
 
 
